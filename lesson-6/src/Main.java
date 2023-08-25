fc0ed60b2e5afe178806b2ea478726948c69a8fc
Aisson-PC <alisson_oliveira_11@hotmail.com>
2023-08-25 16:13:18 -0300

    feat: understanding args and use args with terminal

--- lesson-6/src/Main.java
+++ lesson-6/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Argumentos
+        System.out.println("Você digitou "+ args[0]);
     }
 }
+
+/*
+*   No terminal, abrir o diretório dos arquivos
+*   Digitar: javac Main.java
+*   Após, digitar: java Main "Olá Mundo"
+*/
