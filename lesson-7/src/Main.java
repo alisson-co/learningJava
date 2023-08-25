3b310fac2316ad8b3e4a37dce6c42abb58702ebe
Aisson-PC <alisson_oliveira_11@hotmail.com>
2023-08-25 17:25:10 -0300

    docs: types of errors

--- lesson-7/src/Main.java
+++ lesson-7/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* understanding error */
+
+        // syntax error
+
+        // semantic error
+
+        // execution time error
+
     }
 }
