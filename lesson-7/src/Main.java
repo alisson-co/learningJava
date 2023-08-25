d4aefa5c6cbe111a1edc278a4cd5e8d94f7e5cde
Aisson-PC <alisson_oliveira_11@hotmail.com>
2023-08-25 17:45:31 -0300

    feat: showing error types and adding docs

--- lesson-7/src/Main.java
+++ lesson-7/src/Main.java
@@ -2,11 +2,20 @@ public class Main {
     public static void main(String[] args) {
         /* understanding error */
 
-        // syntax error
+        /*===== SYNTAX ERROR =====*/
+        System.out.println("Olá mundo"); // se faltar algum ; ou "", irá apresentar um erro de sintaxe
 
-        // semantic error
 
-        // execution time error
+        /*===== SEMANTIC ERROR =====*/
+        int cont; // me retorna um erro informando que não inicializei a variável
+
+        cont = 2; // agora inicializando a variável, não retorma mais o erro
+
+        System.out.println("Você digitou: " + cont);
+
+
+        /*===== EXECUTION TIME ERROR =====*/
+        System.out.println("Você digitou: " + 1/0); // irá retornar um erro em tempo de execução, pois nada é divisivel por 0
 
     }
 }
