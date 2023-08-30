f67f4f19243899344a1c04a3c2835db7c6010c69
Aisson-PC <alisson_oliveira_11@hotmail.com>
2023-08-30 17:57:21 -0300

    feat: use switch case update

--- lesson-15/src/Main.java
+++ lesson-15/src/Main.java
@@ -8,37 +8,15 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int weekDay = scan.nextInt();
 
-        switch (weekDay){
-            case 1:
-                System.out.println("Domingo");
-                break;
-
-            case 2:
-                System.out.println("Segunda");
-                break;
-
-            case 3:
-                System.out.println("Terça");
-                break;
-
-            case 4:
-                System.out.println("Quarta");
-                break;
-
-            case 5:
-                System.out.println("Quinta");
-                break;
-
-            case 6:
-                System.out.println("Sexta");
-                break;
-
-            case 7:
-                System.out.println("Sábado");
-                break;
-
-            default:
-                System.out.println("Não é um dia da semana válido!");
+        switch (weekDay) {
+            case 1 -> System.out.println("Domingo");
+            case 2 -> System.out.println("Segunda");
+            case 3 -> System.out.println("Terça");
+            case 4 -> System.out.println("Quarta");
+            case 5 -> System.out.println("Quinta");
+            case 6 -> System.out.println("Sexta");
+            case 7 -> System.out.println("Sábado");
+            default -> System.out.println("Não é um dia da semana válido!");
         }
     }
 }
