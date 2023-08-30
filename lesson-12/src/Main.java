78d91500063ef4b2da861e562ca560162ebb12b9
Aisson-PC <alisson_oliveira_11@hotmail.com>
2023-08-30 11:53:27 -0300

    feat: using or scanner

--- lesson-12/src/Main.java
+++ lesson-12/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // chamando a função de scanner(capturar dados de entrada via terminal)
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe seu nome completo");
+
+        // atribuíndo os dados coletados à variável name
+        String fullName = scan.nextLine();
+
+        System.out.println("Prazer, " + fullName + " e seja bem-vindo.");
     }
 }
