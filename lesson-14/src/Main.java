a4c5cd5d3a3d293ca7917563616668c4977dd8e9
Aisson-PC <alisson_oliveira_11@hotmail.com>
2023-08-30 17:34:32 -0300

    feat: use if else

--- lesson-14/src/Main.java
+++ lesson-14/src/Main.java
@@ -6,14 +6,18 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Informe sua idade: ");
+        System.out.println("Informe o valor do produto: ");
 
-        int age = scan.nextInt();
+        double price = scan.nextDouble();
 
-        if (age >= 18){
-            System.out.println("Você é maior de idade.");
+        if (price <= 10) {
+            System.out.println("Pode comprar, está barato!");
+        } else if (price > 10 && price < 15){
+            System.out.println("Pode pedir um desconto.");
+        } else if (price >= 15 && price < 17) {
+            System.out.println("Melhor pesquisar mais.");
         } else {
-            System.out.println("Você não é maior de idade.");
+            System.out.println("O preço está caro!");
         }
     }
 }
