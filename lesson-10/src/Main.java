2a4e4cee6f421214ec80a077b6061585f530a1c7
AlissonOliveira <alisson_oliveira_11@hotmail.com>
2023-08-29 22:19:29 -0300

    docs: adding docs to primitive type variables

--- lesson-10/src/Main.java
+++ lesson-10/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
+        /* Ao declarar uma variavel, utilizar o padrao de Case Sentive */
+
+        /* Tipos Primitivos */
+        // Numeros inteiros = int (6)
+        // Numeros flutuantes = float (6.8)
+        // Caracter = char ("A")
+
         System.out.println("Hello world!");
     }
 }
