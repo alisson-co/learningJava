1e1beb2888e8d3f33e704040e1854f7e0e986a61
Aisson-PC <alisson_oliveira_11@hotmail.com>
2023-08-30 17:25:30 -0300

    feat: if and else

--- lesson-14/src/Main.java
+++ lesson-14/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* IF ELSE */
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe sua idade: ");
+
+        int age = scan.nextInt();
+
+        if (age >= 18){
+            System.out.println("Você é maior de idade.");
+        } else {
+            System.out.println("Você não é maior de idade.");
+        }
     }
 }
