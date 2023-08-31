fcc3449989cb0489b95a87482782e67853ad87ee
Aisson-PC <alisson_oliveira_11@hotmail.com>
2023-08-31 16:54:06 -0300

    feat: using the while structure

--- lesson-16/src/Main.java
+++ lesson-16/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* while | do while */
+
+        int i = 1;
+        int max = 10;
+
+        System.out.println("Contando de " + i + " até " + max);
+        while (i <= max){
+            System.out.println("Valor do i: " + i);
+            i++;
+        }
+
     }
 }
